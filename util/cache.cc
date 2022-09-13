28b14f368e6d6d10b999db443a0c837e8bd86634
yszhou <987912102@qq.com>
2022-09-13 22:23:35 +0800

    notes about cache{table, LRU}

--- util/cache.cc
+++ util/cache.cc
@@ -37,9 +37,14 @@ namespace {
 // Elements are moved between these lists by the Ref() and Unref() methods,
 // when they detect an element in the cache acquiring or losing its only
 // external reference.
+// 也就是说 LRU仅仅保存 unused node 的顺序
+// 至于用户正在使用中的node，根本不用去管这些 in-used 节点的顺序
 
 // An entry is a variable length heap-allocated structure.  Entries
 // are kept in a circular doubly linked list ordered by access time.
+// 这个LRUHandler 不就是个double-linked list的节点吗？
+// 实际上一个LRU handler 就代表了一个hash-value相同的链表
+// hash-value通过 next_hash 连接起来
 struct LRUHandle {
   void* value;
   void (*deleter)(const Slice&, void* value);
@@ -72,13 +77,16 @@ class HandleTable {
   HandleTable() : length_(0), elems_(0), list_(nullptr) { Resize(); }
   ~HandleTable() { delete[] list_; }
 
+  // 查找是否已有，如果找到则返回对应节点
   LRUHandle* Lookup(const Slice& key, uint32_t hash) {
     return *FindPointer(key, hash);
   }
 
+  // 添加新节点
   LRUHandle* Insert(LRUHandle* h) {
     LRUHandle** ptr = FindPointer(h->key(), h->hash);
     LRUHandle* old = *ptr;
+    // 头插法
     h->next_hash = (old == nullptr ? nullptr : old->next_hash);
     *ptr = h;
     if (old == nullptr) {
@@ -92,6 +100,7 @@ class HandleTable {
     return old;
   }
 
+  // 直接从该bucket中删除该节点
   LRUHandle* Remove(const Slice& key, uint32_t hash) {
     LRUHandle** ptr = FindPointer(key, hash);
     LRUHandle* result = *ptr;
@@ -105,13 +114,17 @@ class HandleTable {
  private:
   // The table consists of an array of buckets where each bucket is
   // a linked list of cache entries that hash into the bucket.
-  uint32_t length_;
-  uint32_t elems_;
+  uint32_t length_; // bucket数量
+  uint32_t elems_; // 当前table中已经保存的元素的数量
   LRUHandle** list_;
+  // list_ 中的节点是bucket，每个bucket都是相同hash的链表
 
   // Return a pointer to slot that points to a cache entry that
   // matches key/hash.  If there is no such cache entry, return a
   // pointer to the trailing slot in the corresponding linked list.
+  // 首先根据hash value找bucket，也就是找到对应的list
+  // 再从对应的list里面找目标节点
+  // 找到之后返回该节点
   LRUHandle** FindPointer(const Slice& key, uint32_t hash) {
     LRUHandle** ptr = &list_[hash & (length_ - 1)];
     while (*ptr != nullptr && ((*ptr)->hash != hash || key != (*ptr)->key())) {
@@ -120,6 +133,7 @@ class HandleTable {
     return ptr;
   }
 
+  // 调整空间
   void Resize() {
     uint32_t new_length = 4;
     while (new_length < elems_) {
@@ -129,6 +143,7 @@ class HandleTable {
     memset(new_list, 0, sizeof(new_list[0]) * new_length);
     uint32_t count = 0;
     for (uint32_t i = 0; i < length_; i++) {
+      // 对每个bucket进行遍历，将所有元素进行rehash
       LRUHandle* h = list_[i];
       while (h != nullptr) {
         LRUHandle* next = h->next_hash;
@@ -177,15 +192,21 @@ class LRUCache {
   bool FinishErase(LRUHandle* e) EXCLUSIVE_LOCKS_REQUIRED(mutex_);
 
   // Initialized before use.
+  // 总容量
   size_t capacity_;
 
   // mutex_ protects the following state.
   mutable port::Mutex mutex_;
+  // 已经存储的数量
   size_t usage_ GUARDED_BY(mutex_);
 
   // Dummy head of LRU list.
   // lru.prev is newest entry, lru.next is oldest entry.
   // Entries have refs==1 and in_cache==true.
+  // 两个都是双向链表
+  // 一个 lru 链表存储 "不再被使用 可以被回收利用的节点"
+  // 一个 in_use 链表存储目前使用中的节点
+  // 单独搞的lru，lru中的节点 一定是 un_used! 只有ref=0的节点才会进入lru中
   LRUHandle lru_ GUARDED_BY(mutex_);
 
   // Dummy head of in-use list.
@@ -204,7 +225,9 @@ LRUCache::LRUCache() : capacity_(0), usage_(0) {
 }
 
 LRUCache::~LRUCache() {
+  // 确保 in_use 为空
   assert(in_use_.next == &in_use_);  // Error if caller has an unreleased handle
+  // 释放lru中的所有节点
   for (LRUHandle* e = lru_.next; e != &lru_;) {
     LRUHandle* next = e->next;
     assert(e->in_cache);
@@ -215,6 +238,9 @@ LRUCache::~LRUCache() {
   }
 }
 
+// 场景: 某个节点被访问
+// 如果是存在于lru中，那么移动到in_use
+// ref++
 void LRUCache::Ref(LRUHandle* e) {
   if (e->refs == 1 && e->in_cache) {  // If on lru_ list, move to in_use_ list.
     LRU_Remove(e);
@@ -223,6 +249,9 @@ void LRUCache::Ref(LRUHandle* e) {
   e->refs++;
 }
 
+// 场景: 某个节点不再被需要
+// ref-- 如果ref==0那么彻底释放节点对应的kvp
+// 否则，移动到lru开头
 void LRUCache::Unref(LRUHandle* e) {
   assert(e->refs > 0);
   e->refs--;
@@ -237,11 +266,13 @@ void LRUCache::Unref(LRUHandle* e) {
   }
 }
 
+// 在当前双向链表中(可能是lru 也可能是in_use)去除掉当前节点
 void LRUCache::LRU_Remove(LRUHandle* e) {
   e->next->prev = e->prev;
   e->prev->next = e->next;
 }
 
+// 将当前节点添加到目标链表表头(目标链表可能是lru 也可能是in_use)
 void LRUCache::LRU_Append(LRUHandle* list, LRUHandle* e) {
   // Make "e" newest entry by inserting just before *list
   e->next = list;
@@ -250,6 +281,8 @@ void LRUCache::LRU_Append(LRUHandle* list, LRUHandle* e) {
   e->next->prev = e;
 }
 
+// 场景: 查找table已有的kvp，存在则返回对应节点
+// 如果存在，则ref++，移动表头并返回节点
 Cache::Handle* LRUCache::Lookup(const Slice& key, uint32_t hash) {
   MutexLock l(&mutex_);
   LRUHandle* e = table_.Lookup(key, hash);
@@ -259,11 +292,16 @@ Cache::Handle* LRUCache::Lookup(const Slice& key, uint32_t hash) {
   return reinterpret_cast<Cache::Handle*>(e);
 }
 
+// 场景: 将目标节点进行释放
+// ref-- 移动开头，必要则释放kvp
 void LRUCache::Release(Cache::Handle* handle) {
   MutexLock l(&mutex_);
   Unref(reinterpret_cast<LRUHandle*>(handle));
 }
 
+// 场景: 在cache中添加新的kvp
+// 构造handler，直接ref++并添加到 in use 中
+// 如果超出了目标容量，则不断从lru中释放内存
 Cache::Handle* LRUCache::Insert(const Slice& key, uint32_t hash, void* value,
                                 size_t charge,
                                 void (*deleter)(const Slice& key,
@@ -281,6 +319,7 @@ Cache::Handle* LRUCache::Insert(const Slice& key, uint32_t hash, void* value,
   e->refs = 1;  // for the returned handle.
   std::memcpy(e->key_data, key.data(), key.size());
 
+  // 直接添加到 in_use 中
   if (capacity_ > 0) {
     e->refs++;  // for the cache's reference.
     e->in_cache = true;
@@ -291,6 +330,7 @@ Cache::Handle* LRUCache::Insert(const Slice& key, uint32_t hash, void* value,
     // next is read by key() in an assert, so it must be initialized
     e->next = nullptr;
   }
+  // 如果 已经使用的数量 超出了容量，就不断删除lru的元素
   while (usage_ > capacity_ && lru_.next != &lru_) {
     LRUHandle* old = lru_.next;
     assert(old->refs == 1);
@@ -305,6 +345,10 @@ Cache::Handle* LRUCache::Insert(const Slice& key, uint32_t hash, void* value,
 
 // If e != nullptr, finish removing *e from the cache; it has already been
 // removed from the hash table.  Return whether e != nullptr.
+// TODO 有点不理解，这里的cache和table有啥区别，既然是个持久化kvp database，那么只需要一个hashtable就够了吧，为什么搞这个lru cache
+// 首先，这里的hash_table是存在于内存中的，不是持久层，而是cache层
+// 既然搞这个lru cache，肯定是为了利用有限的内存，尽可能加速数据读写访问
+// 作用: 从LRU中减少ref_cnt，最终目的是为了释放节点，释放内存
 bool LRUCache::FinishErase(LRUHandle* e) {
   if (e != nullptr) {
     assert(e->in_cache);
@@ -321,6 +365,7 @@ void LRUCache::Erase(const Slice& key, uint32_t hash) {
   FinishErase(table_.Remove(key, hash));
 }
 
+// 释放所有LRU节点占用的内存，LRU中的节点应当满足refs==1
 void LRUCache::Prune() {
   MutexLock l(&mutex_);
   while (lru_.next != &lru_) {
